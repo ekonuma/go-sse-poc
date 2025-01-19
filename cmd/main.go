c99f5d03a8738218f2875a3d551791bf09e76329
ekonuma <ekonuma12@gmail.com>
2025-01-19 23:03:18 +0900

    feat: Add SSE send message to rabbitmq

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/ekonuma/go-sse-poc/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	out := make(chan amqp.Delivery)
+	rabbitmqChannel, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+
+	go rabbitmq.Consume("mmessages", rabbitmqChannel, out)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "html/index.html")
+	})
+	http.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		for message := range out {
+			fmt.Fprintf(w, "event: message\n")
+			fmt.Fprintf(w, "Data: %s\n\n", message.Body)
+			w.(http.Flusher).Flush()
+		}
+	})
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ekonuma/go-sse-poc
 
 go 1.21.13
+
+require github.com/rabbitmq/amqp091-go v1.10.0
